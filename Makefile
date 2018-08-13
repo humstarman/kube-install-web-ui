f79ea473a8b2d0781274cf6fedbb1b31b95fd353
humstarman <lueyoung7@gmail.com>
2018-08-13 10:17:50 +0800

    init

--- Makefile
+++ Makefile
@@ -1,2 +1,24 @@
+VER=v8.11.3
+ENV_FILE=/etc/profile
+NODE_HOME=/usr/local/node
+
+all: download unzip install config test install-express
+
+download:
+	@[ -f node-${VER}.tar.gz ] || curl -O https://nodejs.org/dist/${VER}/node-${VER}.tar.gz
+
+unzip:
+	@[ -f node-${VER}.tar.gz ] || tar -zxvf node-${VER}.tar.gz
+
+install:
+	@cd node-${VER} && ./configure –prefix=${NODE_HOME} && make && make install
+
+config:
+	@./scripts/config-env.sh -e ${ENV_FILE} -n ${NODE_HOME}
+	@source ${ENV_FILE}
+
 test:
-	@echo test
+	@node -v
+
+install-express:
+	@npm install -g express-generator
---
+++ scripts/config-env.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+show_help () {
+cat << USAGE
+usage: $0 [ -e ENV-SETTING-FILE ] [ -n NODE-HOME ]
+    -e : Specify the environment file.
+    -n : Specify the value of NODE_HOME. 
+ 
+USAGE
+exit 0
+}
+# Get Opts
+while getopts "he:n:" opt; do # 选项后面的冒号表示该选项需要参数
+    case "$opt" in
+    h)  show_help
+        ;;
+    e)  ENV_FILE="$REVERSE_CIDRS $OPTARG" # 参数存在$OPTARG中
+        ;;
+    n)  NODE_HOME=$OPTARG
+        ;;
+    ?)  # 当有不认识的选项的时候arg为?
+        echo "unkonw argument"
+        exit 1
+        ;;
+    esac
+done
+[ -z "$*" ] && show_help
+chk_var () {
+if [ -z "$2" ]; then
+  echo "$(date -d today +'%Y-%m-%d %H:%M:%S') - [ERROR] - no input for \"$1\", try \"$0 -h\"."
+  sleep 3
+  exit 1
+fi
+}
+chk_var -e ${ENV_FILE}
+chk_var -n ${NODE_HOME}
+[ -f $ENV_FILE ] || touch $ENV_FILE
+if cat ${ENV_FILE} | grep "export NODE_HOME=" >/dev/null 2>&1; then
+  TMP=$(echo ${NODE_HOME} | tr "/" "?")
+  #echo $TMP
+  N=$(cat ${ENV_FILE} | grep -n "export NODE_HOME=")
+  N=${N%%:*}
+  #echo $N
+  sed -i "${N}c export NODE_HOME=${TMP}" ${ENV_FILE}
+  sed -i s^"?"^"/"^g ${ENV_FILE}
+  exit 0
+fi
+cat >> ${ENV_FILE}<<EOF
+export NODE_HOME=${NODE_HOME}
+EOF
+cat >> ${ENV_FILE}<<"EOF"
+export PATH=$NODE_HOME/bin:$PATH
+EOF
